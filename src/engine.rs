1e40310eaaaefcb5da6c02abbed135a0418bb971
lemunozm <lemunozm@gmail.com>
2022-03-30 19:06:47 +0200

    Reorganized and simplified engine

--- src/engine.rs
+++ src/engine.rs
@@ -1,20 +1,46 @@
-use crate::channel::{Receiver, Sender};
+use crate::channel::{ClosedChannel, Receiver, Sender};
 use crate::interface::{InputConnector, Message, OutputConnector, Service};
 
-use tokio::sync::{mpsc, mpsc::error::SendError};
+use tokio::{
+    sync::mpsc,
+    task::{JoinError, JoinHandle},
+};
 
 use std::collections::HashMap;
 use std::collections::HashSet;
 
 struct ServiceConfig {
     name: String,
-    builder: Box<dyn Fn() -> Box<dyn Service + Send> + Send>,
+    service: Box<dyn Service + Send>,
     whitelist: Option<HashSet<String>>,
 }
 
 struct ServiceHandle {
     whitelist: Option<HashSet<String>>,
     input_sender: mpsc::Sender<Message>,
+}
+
+impl ServiceHandle {
+    async fn process_message(&self, message: Message) {
+        let allowed = match &self.whitelist {
+            Some(whitelist) => whitelist.contains(&message.user),
+            None => true,
+        };
+
+        if allowed {
+            let service_name = message.service.clone();
+            match self.input_sender.send(message).await {
+                Ok(()) => log::info!("Processing message for service '{}'", service_name),
+                Err(_) => log::warn!("Drop message for removed service '{}'", service_name),
+            }
+        } else {
+            log::warn!(
+                "Drop message for service '{}' not allowed for user '{}'",
+                message.service,
+                message.user,
+            );
+        }
+    }
 }
 
 #[derive(Default)]
@@ -38,11 +64,11 @@ impl Engine {
     pub fn add_service(
         mut self,
         name: impl Into<String>,
-        service: impl Service + Send + Clone + 'static,
+        service: impl Service + Send + 'static,
     ) -> Engine {
         self.service_configs.push(ServiceConfig {
             name: name.into(),
-            builder: Box::new(move || Box::new(service.clone())),
+            service: Box::new(service),
             whitelist: None,
         });
         self
@@ -51,12 +77,12 @@ impl Engine {
     pub fn add_service_for<S: Into<String>>(
         mut self,
         name: impl Into<String>,
-        service: impl Service + Send + Clone + 'static,
+        service: impl Service + Send + 'static,
         whitelist: impl IntoIterator<Item = S>,
     ) -> Engine {
         self.service_configs.push(ServiceConfig {
             name: name.into(),
-            builder: Box::new(move || Box::new(service.clone())),
+            service: Box::new(service),
             whitelist: Some(whitelist.into_iter().map(|s| s.into()).collect()),
         });
         self
@@ -66,58 +92,82 @@ impl Engine {
         log::info!("Initializing engine...");
 
         let (input_sender, mut input_receiver) = mpsc::channel(32);
-        tokio::spawn(async move {
-            let task =
-                tokio::spawn(async move { self.input.unwrap().run(Sender(input_sender)).await });
-
-            log::info!("Loading input connector");
-
-            match task.await {
-                Ok(Ok(())) => log::info!("Input connector down (finished)"),
-                Ok(Err(_)) => log::info!("Input connector down (disconnected)"),
-                Err(_) => log::error!("Input connector down (panicked)"),
-            }
-        });
+        Self::load_input(self.input.unwrap(), input_sender);
 
         let (output_sender, output_receiver) = mpsc::channel(32);
-        let mut output_task = tokio::spawn(async move {
-            let task =
-                tokio::spawn(
-                    async move { self.output.unwrap().run(Receiver(output_receiver)).await },
-                );
+        let mut output_task = Self::load_output(self.output.unwrap(), output_receiver);
 
+        let services = Self::load_services(self.service_configs, output_sender);
+
+        loop {
+            tokio::select! {
+                Some(message) = input_receiver.recv() => {
+                    match services.get(&message.service) {
+                        Some(handle) => handle.process_message(message).await,
+                        None => log::warn!("Drop Message for unknown service '{}'", message.service),
+                    }
+                }
+                _ = &mut output_task => break,
+                else => break,
+            }
+        }
+    }
+
+    fn load_input(
+        input: Box<dyn InputConnector + Send>,
+        sender: mpsc::Sender<Message>,
+    ) -> JoinHandle<()> {
+        tokio::spawn(async move {
+            log::info!("Loading input connector");
+
+            let result = tokio::spawn(async move { input.run(Sender(sender)).await }).await;
+
+            Self::log_join_result(result, "Input connector");
+        })
+    }
+
+    fn load_output(
+        output: Box<dyn OutputConnector + Send>,
+        receiver: mpsc::Receiver<Message>,
+    ) -> JoinHandle<()> {
+        tokio::spawn(async move {
             log::info!("Loading output connector");
 
-            match task.await {
-                Ok(Ok(())) => log::info!("Output connector down (finished)"),
-                Ok(Err(_)) => log::info!("Output connector down (disconnected)"),
-                Err(_) => log::error!("Output connector down (panicked)"),
-            }
-        });
+            let result = tokio::spawn(async move { output.run(Receiver(receiver)).await }).await;
 
-        let services: HashMap<String, ServiceHandle> = self
-            .service_configs
+            Self::log_join_result(result, "Output connector");
+        })
+    }
+
+    fn load_service(
+        service: Box<dyn Service + Send>,
+        receiver: mpsc::Receiver<Message>,
+        sender: mpsc::Sender<Message>,
+        name: String,
+    ) -> JoinHandle<()> {
+        tokio::spawn(async move {
+            log::info!("Loading service '{}'", name);
+
+            let result =
+                tokio::spawn(async move { service.run(Receiver(receiver), Sender(sender)).await })
+                    .await;
+
+            Self::log_join_result(result, &format!("Service '{}'", name));
+        })
+    }
+
+    fn load_services(
+        configs: Vec<ServiceConfig>,
+        output_sender: mpsc::Sender<Message>,
+    ) -> HashMap<String, ServiceHandle> {
+        let services = configs
             .into_iter()
             .map(|config| {
                 let (input_sender, input_receiver) = mpsc::channel(32);
                 let output_sender = output_sender.clone();
-                let service = (config.builder)();
                 let service_name = config.name.clone();
-                tokio::spawn(async move {
-                    let task = tokio::spawn(async move {
-                        service
-                            .run(Receiver(input_receiver), Sender(output_sender))
-                            .await
-                    });
 
-                    log::info!("Loading service '{}'", service_name);
-
-                    match task.await {
-                        Ok(Ok(())) => log::info!("Service '{}' down (finished)", service_name),
-                        Ok(Err(_)) => log::info!("Service '{}' down (disconnected)", service_name),
-                        Err(_) => log::error!("Service '{}' down (panicked)", service_name),
-                    }
-                });
+                Self::load_service(config.service, input_receiver, output_sender, service_name);
 
                 (
                     config.name,
@@ -131,25 +181,14 @@ impl Engine {
 
         drop(output_sender);
 
-        loop {
-            tokio::select! {
-                input_message = input_receiver.recv() => {
-                    let input_message = input_message.unwrap();
-                    if let Some(handle) = services.get(&input_message.service) {
-                        let allowed = match &handle.whitelist {
-                            Some(whitelist) => whitelist.contains(&input_message.user),
-                            None => true,
-                        };
+        services
+    }
 
-                        if allowed {
-                            if let Err(SendError(message)) = handle.input_sender.send(input_message).await {
-                                log::error!("Drop message for removed service '{}'", message.service);
-                            }
-                        }
-                    }
-                }
-                _ = &mut output_task => break,
-            }
+    fn log_join_result(result: Result<Result<(), ClosedChannel>, JoinError>, name: &str) {
+        match result {
+            Ok(Ok(())) => log::info!("{} down (finished)", name),
+            Ok(Err(_)) => log::info!("{} down (disconnected)", name),
+            Err(_) => log::error!("{} down (panicked)", name),
         }
     }
 }
