12e9dda732b52feee340bb22fb62f9c9318d3bfd
lemunozm <lemunozm@gmail.com>
2022-04-03 13:04:24 +0200

    Process service refactor

--- src/services/process.rs
+++ src/services/process.rs
@@ -16,45 +16,42 @@ impl Service for Process {
     ) -> Result<(), ClosedChannel> {
         loop {
             let request = input.recv().await?;
-
-            let mut program_args = request.args.iter();
-            match program_args.next() {
-                Some(arg0) => {
-                    let child = Command::new(arg0).args(program_args).output();
-                    tokio::spawn({
-                        let output = output.clone();
-                        async move {
-                            let cmd_str = request.args.join(" ");
-                            if let Ok(child_output) = child.await {
-                                let response = Message::response(&request)
-                                    .args([format!(
-                                        "Terminated ({}): {}",
-                                        child_output.status, cmd_str
-                                    )])
-                                    .body(
-                                        std::str::from_utf8(&child_output.stdout)
-                                            .unwrap_or("[binary]"),
-                                    );
-
-                                output.send(response).await.ok();
-                            } else {
-                                let response = Message::response(&request)
-                                    .args(["error"])
-                                    .body(format!("Error while running: {}", cmd_str));
-
-                                output.send(response).await.ok();
-                            }
-                        }
-                    });
-                }
+            match request.args.get(0) {
+                Some(_) => spawn_process(request, output.clone()),
                 None => {
                     let response = Message::response(&request)
                         .args(["format error"])
                         .body(format!("You need to specify a process to run"));
 
-                    output.send(response).await.ok();
+                    output.send(response).await?;
                 }
             }
         }
     }
+}
+
+fn spawn_process(request: Message, output: Sender) {
+    let mut program_args = request.args.iter();
+    let arg0 = program_args.next().unwrap();
+    let child = Command::new(arg0).args(program_args).output();
+
+    tokio::spawn({
+        let output = output.clone();
+        async move {
+            let cmd_str = request.args.join(" ");
+            if let Ok(child_output) = child.await {
+                let response = Message::response(&request)
+                    .args([format!("Terminated ({}): {}", child_output.status, cmd_str)])
+                    .body(std::str::from_utf8(&child_output.stdout).unwrap_or("[binary]"));
+
+                output.send(response).await.ok();
+            } else {
+                let response = Message::response(&request)
+                    .args(["error"])
+                    .body(format!("Error while running: {}", cmd_str));
+
+                output.send(response).await.ok();
+            }
+        }
+    });
 }
